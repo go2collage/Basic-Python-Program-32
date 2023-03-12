4f18d2c0a54bd6e8535564434710c5804a9bc45c
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 14:53:27 +0530

    Add files via upload

---
+++ BP_P32.py
@@ -0,0 +1,25 @@
+# set in Python
+
+# create set
+set1 = set()
+print("set1 type: ", type(set1))
+
+# add items in set
+set1.add(60)
+print("set1 is: ", set1)
+
+set1.add(50)
+print("set1 is: ", set1)
+
+set1.add(70)
+print("set1 is: ", set1)
+
+# Auto manage number in ascending order
+
+set1.discard(60)
+print("After remove item from set1 is: ", set1)
+
+# add() method used to add items in set
+# discard() method is used to remove items from set
+
+# Thnaks for Watching
